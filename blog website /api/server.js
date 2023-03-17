5ca42259b285713f2974e18f5da172790d928f61
Caption-D <drumilhved@gamil.com>
2023-03-17 15:16:18 +0530

    added introductionPage, JoinRoomPage, RoomPage, Store and everything is working now

--- blog website /api/server.js
+++ blog website /api/server.js
@@ -1,11 +1,27 @@
 const express  = require("express");
 const mongoose = require("mongoose");
-const mongodbUrl = process.env.DATABASE_URL;
+const bodyParser = require('body-parser');
+
+//ROUTING work going on here
+const routes = require('./routes/routes')
+const homeRoute = require('./routes/homeRoute');
 const app = express();
-app.get("/", function(req, res){
-    res.send("<h1>Hello world</h1>");
-});
+
+//Environment (.env) variables to import 
+const mongodbUrl = process.env.DATABASE_URL;
+
+//Imorting database here 
+const database = require('./database');
+// import {giveBlog} from './database'
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(routes);
+app.use(homeRoute);
+app.use(database);
+
+const blog1 = database.giveBlog();
+console.log(blog1);
+
 app.listen(3000, function(err){
     console.log("The server is up and running!!");
-    
-})
+
+});
